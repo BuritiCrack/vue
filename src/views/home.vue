f60626cb517f1c0346e7d2775989e9a8accc5f77
BuritiCrack <65743671+BuritiCrack@users.noreply.github.com>
2023-03-11 11:41:43 -0500

    Initial commit

---
+++ src/views/home.vue
@@ -0,0 +1,30 @@
+<!-- Home -->
+
+<template>
+  <div>
+    home
+  </div>
+
+</template>
+
+<script>
+import { ref } from 'vue';
+async function getCharacters() {
+  const response = await fetch('https://rickandmortyapi.com/api/character');
+  const data = await response.json();
+  return data.results;
+}
+  export default {
+    name: 'HomeView',
+  setup() {
+    const characters = ref([]);
+
+    return {
+      characters,
+    };
+  },
+  async created() {
+    this.characters = await getCharacters();
+  },
+};
+</script>
